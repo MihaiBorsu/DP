743824716e9a423cf874e29fcd52c4b939637b34
MihaiBorsu <mihai.borsu@gmail.com>
2020-03-11 22:02:41 +0200

    second commit

--- login.c
+++ login.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sqlite3.h>
 
 sqlite3 *db;
@@ -14,10 +15,12 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
    return 0;
 }
 
-static int callback2(void *data, int argc, char **argv, char **azColName) {
+static char* callback2(void *data, int argc, char **argv, char **azColName) {
    int i;
-   fprintf(stderr, "%s: ", (const char*)data);
+   //char result[255] = "";
+   fprintf(stderr, "%s: \n", (const char*)data);
    for(i = 0; i<argc; i++) {
+      //strcat(result, argv[i] ? argv[i] : "NULL");
       printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
    }
    printf("\n");
@@ -25,11 +28,21 @@ static int callback2(void *data, int argc, char **argv, char **azColName) {
 }
 
 void create_table(){
-  char *zErrMsg = 0;
-  char *sql = "CREATE TABLE USERS("	     \
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+
+   /* Open database */
+   rc = sqlite3_open("test.db", &db);
+   if( rc ) 
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+   else 
+      fprintf(stdout, "Opened database successfully\n");
+   
+
+  char *sql = "CREATE TABLE USERS("      \
       "ID INT PRIMARY KEY NOT NULL," \
       "NAME TEXT NOT NULL," \
-      "PASSWORD TEXT NOT NULL," \
       "IP CHAR(16) NOT NULL," \
       "MAC CHAR(50) NOT NULL );";
 
@@ -44,28 +57,40 @@ void create_table(){
 
 }
 
-void insert(){
+
+void my_insert(char *sql){
+   sqlite3 *db;
    char *zErrMsg = 0;
-
-  char *sql = "INSERT INTO USERS (ID,NAME,PASSWORD,IP,MAC) " \
-    "VALUES (5, 'Paul', '32', 'California', '20000.00' ); ";
-
-
-  
-  
-  rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-  if( rc != SQLITE_OK ) {
+   int rc;
+   /* Open database */
+   rc = sqlite3_open("test.db", &db);
+   if( rc ) 
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    else 
+      fprintf(stderr, "Opened database successfully\n");
+     
+   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+   if( rc != SQLITE_OK ) {
       fprintf(stderr, "SQL error: %s\n", zErrMsg);
       sqlite3_free(zErrMsg);
-  } else {
+   } else {
       fprintf(stdout, "Records created successfully\n");
-  }
+   }
+   sqlite3_close(db);
 }
 
-static void select2(){
-  char *zErrMsg = 0;
-  const char* data = "Callback function called";
-   char *sql = "SELECT * from USERS";
+void my_select(char *sql){
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+   const char* data = "Callback function called";
+
+   /* Open database */
+   rc = sqlite3_open("test.db", &db);
+   if( rc ) 
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    else 
+      fprintf(stderr, "Opened database successfully\n");   
    /* Execute SQL statement */
    rc = sqlite3_exec(db, sql, callback2, (void*)data, &zErrMsg);
    if( rc != SQLITE_OK ) {
@@ -74,21 +99,88 @@ static void select2(){
    } else {
       fprintf(stdout, "Operation done successfully\n");
    }
+   sqlite3_close(db);
+}
+
+
+static int check_if_user_exists(char *user){
+  char *sql=malloc(strlen("SELECT NAME FROM USERS WHERE NAME = ")+strlen(user));
+
+  strcat(sql,user);
+  strcat(sql,"\n");
+  printf("%s",sql);
+
+  /* Open database */
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+   rc = sqlite3_open("test.db", &db);
+   if( rc ) 
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    else 
+      fprintf(stderr, "Opened database successfully\n"); 
+
+  //check for existance
+  struct sqlite3_stmt *selectstmt;
+  int result = sqlite3_prepare_v2(db, sql, -1, &selectstmt, NULL);
+  if(result == SQLITE_OK)
+  {
+     if (sqlite3_step(selectstmt) == SQLITE_ROW)
+        return 1;
+     else
+        return 0;
+  }
+  sqlite3_finalize(selectstmt);
+
+  free(sql);
+  return 0;
 }
 
 int main(void) {
-  char *zErrMsg = 0;
-  rc = sqlite3_open("test.db", &db);
-   
-  if( rc ) {
-     fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-  }
-  else {
-     fprintf(stderr, "Opened database successfully\n");
-  }
+  
 
+  puts("insert username:");
+  char username[20];
+  scanf("%s",username);
+  printf("%s \n", username);
+
+  // check_if_user_exists(username) ? my_insert(): puts()
+  
+  // char *sql = "INSERT INTO USERS (ID, NAME, IP, MAC)" \
+  //       "VALUES (5,'MIHAI','SOMEIP','SOMEMAC')";
+
+  // my_insert(sql);
+
+  char *sql2 = "SELECT * FROM USERS WHERE NAME = 'MIHAI'";
+  
+  //my_select(sql2);
+  printf("%d \n",check_if_user_exists(username));
   
   //create_table();
-  insert();
-  select2();
+  //select2();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//creating database
+  // char *zErrMsg = 0;
+  // rc = sqlite3_open("test.db", &db);
+   
+  // if( rc ) {
+  //    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+  // }
+  // else {
+  //    fprintf(stderr, "Opened database successfully\n");
+  // }
